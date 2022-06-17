117ed807d3877598ff03c688126f324a03366e25
skandertebo <alexandertebourb@gmail.com>
2022-06-17 05:08:35 +0100

    first version of the app

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
 
-const items = [];
+const items = ["eat food" , "make bed" , "shower"];
 const Today = new Date();
 const options = { weekday: 'long' };
 const date = Today.toLocaleDateString("en-US" , options);
 
-app.set('view engine', 'ejs');
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
@@ -22,9 +23,13 @@ app.use(bodyParser.urlencoded({
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs',{date:date});
+    res.render('index.ejs',{date:date , items:items});
 })
 
+app.post('/', (req, res) => {
+    items.push(req.body.item);
+    res.redirect('/');
+})
 
 app.listen(3000 , function () {
     console.log('listening on port 3000');
